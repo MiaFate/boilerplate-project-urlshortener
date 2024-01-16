b48872d7f410263eb76260944a88efac14cfe885
miafate <miapurplefroot@gmail.com>
2024-01-16 20:37:56 -0300

    testing another approach

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const dns = require("node:dns")
 
 // Basic Configuration
+const urlParser = require('url');
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 const port = process.env.PORT || 3000;
@@ -24,8 +25,17 @@ app.get('/', function(_, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(_, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(_, res) {
+  const { url } = req.body
+  dns.lookup(urlParser.parse(url).hostname, async (err, address) => {
+    if (!address) {
+      res.status(200).json({ error: "invalid url" })
+    } else {
+      let newUrl = new Url({ url })
+      const doc = await newUrl.save()
+      res.json({ original_url: url, short_url: doc._id })
+    }
+  })
 });
 app.get('/api/shorturl/:shorturl', async function(req, res) {
   try {
@@ -38,32 +48,32 @@ app.get('/api/shorturl/:shorturl', async function(req, res) {
   }
 })
 
-app.post('/api/:shorturl', async function(req, res) {
-  try {
-    const { url } = req.body;
+// app.post('/api/:shorturl', async function(req, res) {
+//   try {
+//     const { url } = req.body;
 
-    const hostname = new URL(url).hostname;
-    if (hostname.origin === "null") {
-      res.status(200).json({ error: "invalid url" })
-    } else {
-      const isUrlValid = dns.lookup(hostname, (err, addr) => {
-        if (err || !addr) {
-          return false
-        }
-        return true
-      })
-      if (isUrlValid) {
-        let newUrl = new Url({ url })
-        const doc = await newUrl.save()
-        res.json({ original_url: url, short_url: doc._id })
-      } else {
-        res.status(200).json({ error: "invalid url" })
-      }
-    }
-  } catch (error) {
-    res.status(200).json({ error: "invalid url" })
-  }
-})
+//     const hostname = new URL(url).hostname;
+//     if (hostname.origin === "null") {
+//       res.status(200).json({ error: "invalid url" })
+//     } else {
+//       const isUrlValid = dns.lookup(hostname, (err, addr) => {
+//         if (err || !addr) {
+//           return false
+//         }
+//         return true
+//       })
+//       if (isUrlValid) {
+//         let newUrl = new Url({ url })
+//         const doc = await newUrl.save()
+//         res.json({ original_url: url, short_url: doc._id })
+//       } else {
+//         res.status(200).json({ error: "invalid url" })
+//       }
+//     }
+//   } catch (error) {
+//     res.status(200).json({ error: "invalid url" })
+//   }
+// })
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
