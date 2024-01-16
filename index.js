e409756c379a93281c047607c026ca5797914616
miafate <miapurplefroot@gmail.com>
2024-01-16 14:07:33 -0300

    prueba de post

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose')
 const cors = require('cors');
 const app = express();
 
 // Basic Configuration
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
 const port = process.env.PORT || 3000;
 
+//bd Configuration
+mongoose.connect(process.env.MONGO_URI)
+
+const Url = require("./models/urls.js")
+// const createAndSaveUrl = (done) => {
+//   let newUrl = new Url({ url })
+// }
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function(_, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function(_, res) {
   res.json({ greeting: 'hello API' });
 });
+app.get('/api/:shorturl', function(_, res) {
+  res.json({ original_url: "", short_url: "" })
+})
+
+app.post('/api/:shorturl', async function(req, res) {
+  try {
+    const { url } = req.body;
+    let newUrl = new Url({ url })
+    const doc = await newUrl.save()
+    res.json({ original_url: url, short_url: doc._id })
+  } catch (error) {
+    res.json({ error })
+  }
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
